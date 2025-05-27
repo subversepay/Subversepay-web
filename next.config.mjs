59cfab0ac9b7eb702a27052c6393bd41bba7e8a6
mubarak usman <mubarak725908@gmail.com>
2025-05-27 22:47:04 +0100

    change export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export', // Enable static export
+  output: 'export', // Enable static export
   basePath: '/Subversepay-web',
   eslint: {
     ignoreDuringBuilds: true,
